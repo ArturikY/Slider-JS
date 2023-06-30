74372c332665a84c57a67687843dbd73fa5d38a5
Artur <artur090806@yandex.ru>
2023-06-30 11:21:30 +0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,37 @@
-const slides = document.querySelectorAll('.slide')
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+const container = document.querySelector('.container')
 
-const clearActiveClasses = () => {
-	slides.forEach(slide => {
-		slide.classList.remove('active')
-	})
-}
+let activeSlideIndex = 0
 
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
-		slide.classList.toggle('active')
-	})
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+	changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+	changeSlide('down')
+})
+
+const changeSlide = direction => {
+	if (direction === 'up') {
+		activeSlideIndex++
+		if (activeSlideIndex === slidesCount) {
+			activeSlideIndex = 0
+		}
+	} else if (direction === 'down') {
+		activeSlideIndex--
+		if (activeSlideIndex < 0) {
+			activeSlideIndex = 3
+		}
+	}
+
+	const height = container.clientHeight
+
+	mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+	sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
